1501cd3af6b6494e0965cc7ef983708ffcc6e876
Mithun Ayachit <m0t0rbr3th@gmail.com>
2010-01-24 21:05:05 -0600

    added Tests + Pod

--- lib/URI/Encode.pm
+++ lib/URI/Encode.pm
@@ -102,52 +102,87 @@ This document describes URI::Encode version 0.0.1
 	use URI::Encode qw(uri_encode uri_decode);
 	my $encoded = uri_encode($url);
 	my $decoded = uri_decode($url);
-  
+
 =head1 DESCRIPTION
 
-This modules provides simple URI encoding/decoding.
+This modules provides simple URI (Percent) encoding/decoding. This module
+borrows from L<URI::Escape>, but 'tinier'
 
 =head1 METHODS
 
-=over
+=head2 new()
 
-=item new()
+Creates a new object, no argumetns are required
 
-=item encode($url)
+	my $encoder = URI::Encode->new();
 
-=item encode($url, $including_reserved)
+=head2 encode($url, $including_reserved)
 
-=item decode($url)
+This method encodes the URL provided. The method does not encode any
+L</"Reserved Characters"> unless C<$including_reserved> is true. The $url
+provided is first converted into UTF-8 before percent encoding.
 
-=back
+	$uri->encode("http://perl.com/foo bar");      # http://perl.com/foo%20bar
+	$uri->encode("http://perl.com/foo bar", 1);   # http%3A%2F%2Fperl.com%2Ffoo%20bar
+
+=head2 decode($url)
+
+This method decodes a 'percent' encoded URL. If you had encoded the URL using
+this module (or any other method), chances are that the URL was converted to
+UTF-8 before 'percent' encoding. Be sure to check the format and convert back
+if required.
+
+	$uri->decode("http%3A%2F%2Fperl.com%2Ffoo%20bar"); # "http://perl.com/foo bar"
 
 =head1 EXPORTED FUNCTIONS
 
-=over
+The following functions are exported upon request. This provides a non-OOP
+interface
 
-=item uri_encode($url)
+=head2 uri_encode($url, $including_reserved)
 
-=item uri_encode($url, $including_reserved)
+See L</encode($url, $including_reserved)>
 
-=item uri_decode($url)
+=head2 uri_decode($url)
 
-=back
+See L</decode($url)>
+
+=head1 CHARACTER CLASSES
+
+=head2 Reserved Characters
+
+The following characters are considered as reserved (L<RFC
+3986|http://tools.ietf.org/html/rfc3986>)
+
+	 - _ . ~ ! * ' ( ) ; : @ & = + $ , / ? % # [ ]
+
+=head2 Unreserved Characters
+
+The following characters are considered as Unreserved. They will not be encoded
+
+	a-z
+	A-Z
+	0-9
+	- _ . ~
 
 =head1 DEPENDENCIES
 
 L<Encode>
 
-=head1 INCOMPATIBILITIES
+=head1 SEE ALSO
 
-None reported.
+L<RFC 3986|http://tools.ietf.org/html/rfc3986>
+
+L<URI::Escape>
+
+L<Tie::UrlEncoder>
 
 =head1 BUGS AND LIMITATIONS
 
 No bugs have been reported.
 
-Please report any bugs or feature requests to
-C<bug-uri-encode@rt.cpan.org>, or through the web interface at
-L<http://rt.cpan.org>.
+Please report any bugs or feature requests to C<bug-uri-encode@rt.cpan.org>, or
+through the web interface at L<http://rt.cpan.org>.
 
 =head1 AUTHOR
 
@@ -155,30 +190,30 @@ Mithun Ayachit  C<< <mithun@cpan.org> >>
 
 =head1 LICENCE AND COPYRIGHT
 
-Copyright (c) 2010, Mithun Ayachit C<< <mithun@cpan.org> >>. All rights reserved.
+Copyright (c) 2010, Mithun Ayachit C<< <mithun@cpan.org> >>. All rights
+reserved.
 
-This module is free software; you can redistribute it and/or
-modify it under the same terms as Perl itself. See L<perlartistic>.
+This module is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself. See L<perlartistic>.
 
 =head1 DISCLAIMER OF WARRANTY
 
-BECAUSE THIS SOFTWARE IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY
-FOR THE SOFTWARE, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN
-OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES
-PROVIDE THE SOFTWARE "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
-EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE
-ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE SOFTWARE IS WITH
-YOU. SHOULD THE SOFTWARE PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL
-NECESSARY SERVICING, REPAIR, OR CORRECTION.
+BECAUSE THIS SOFTWARE IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY FOR THE
+SOFTWARE, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN OTHERWISE
+STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES PROVIDE THE
+SOFTWARE "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS FOR A PARTICULAR PURPOSE. THE ENTIRE RISK AS TO THE QUALITY AND
+PERFORMANCE OF THE SOFTWARE IS WITH YOU. SHOULD THE SOFTWARE PROVE DEFECTIVE,
+YOU ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR, OR CORRECTION.
 
-IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING
-WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MAY MODIFY AND/OR
-REDISTRIBUTE THE SOFTWARE AS PERMITTED BY THE ABOVE LICENCE, BE
-LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL,
-OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE
-THE SOFTWARE (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR DATA BEING
-RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES OR A
-FAILURE OF THE SOFTWARE TO OPERATE WITH ANY OTHER SOFTWARE), EVEN IF
-SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF
-SUCH DAMAGES.
+IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING WILL ANY
+COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MAY MODIFY AND/OR REDISTRIBUTE THE
+SOFTWARE AS PERMITTED BY THE ABOVE LICENCE, BE LIABLE TO YOU FOR DAMAGES,
+INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING
+OUT OF THE USE OR INABILITY TO USE THE SOFTWARE (INCLUDING BUT NOT LIMITED TO
+LOSS OF DATA OR DATA BEING RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR
+THIRD PARTIES OR A FAILURE OF THE SOFTWARE TO OPERATE WITH ANY OTHER SOFTWARE),
+EVEN IF SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH
+DAMAGES.
+
---
+++ t/00_basic.t
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Encode;
+use Test::More tests => 10;
+
+BEGIN { use_ok("URI::Encode", qw(uri_encode uri_decode)); }
+
+diag("Testing URI::Encode $URI::Encode::VERSION");
+
+# Define URI's
+my $url = "http://mithun.aÿachit.com/my pages.html?name=m!thun&Yours=w%hat?#";
+my $encoded = "http://mithun.a%C3%83%C2%BFachit.com/my%20pages.html?name=m!thun&Yours=w%hat?#";
+my $encoded_reserved = "http%3A%2F%2Fmithun.a%C3%83%C2%BFachit.com%2Fmy%20pages.html%3Fname%3Dm%21thun%26Yours%3Dw%25hat%3F%23";
+
+# Test OOP
+my $uri = new_ok("URI::Encode");
+can_ok($uri, qw(encode decode));
+ok($uri->encode($url) eq $encoded);
+ok($uri->encode($url, 1) eq $encoded_reserved);
+ok(Encode::decode('utf-8-strict', $uri->decode($encoded)) eq $url);
+
+## Test Methods
+can_ok("URI::Encode", qw(uri_encode uri_decode));
+ok(uri_encode($url) eq $encoded);
+ok(uri_encode($url, 1) eq $encoded_reserved);
+ok(Encode::decode('utf-8-strict', uri_decode($encoded)) eq $url);
